87f37b7da9b5fe398941d5a8b7d74276cf56a90f
lattai <leah.attai@gmail.com>
2021-11-03 10:18:52 -0400

    Add db_conncect file

---
+++ db_connect.php
@@ -0,0 +1,24 @@
+<?php
+
+$host="cray.cc.gettysburg.edu"; // vars start w $
+$dbase="f21_1"; //database name
+$user="attale01";
+$pass="attale01";
+$user="farrro01";
+$pass="farrro01";
+$user="tranhi01";
+$pass="tranhi01";
+
+try {
+	
+	$db=new PDO("mysql:host=$host;dbname=$dbase",$user,$pass); // NO SPACES, ~all variables are global
+}
+catch(PDOException $e) {
+ 	// . : string concatenation
+ 	// -> insead of . for remote access
+ 	// ' ' + e.getMessage   -- JAVA
+ 	// ' ' . e-> getMessge  -- PHP
+ 	die("ERROR connecting to MySQL server" . $e->getMessage());
+}
+
+?>

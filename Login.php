0d7e3fb6d7dfc4c782667b7928df8bdbd27c2678
farrro01 <93662240+farrro01@users.noreply.github.com>
2021-11-13 16:51:29 -0500

    Update Login.php

--- Login.php
+++ Login.php
@@ -28,7 +28,7 @@ $uid = 05; //for testing purposes only
 	<form>
 		<label for="username">Username:</label>
         	<input type="text" id="username" name="username"> <br>
-        	<label for="passwird">Password:</label>
+        	<label for="password">Password:</label>
         	<input type="text" id="password" name="password"> <br>
         	<input type="submit" value="Login">
         </form>

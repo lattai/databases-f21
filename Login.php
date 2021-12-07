86b196ca8fcab8998cddbe2e54dd0e55634c3986
farrro01 <93662240+farrro01@users.noreply.github.com>
2021-12-07 18:21:06 -0500

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,30 +1,41 @@
 <!DOCTYPE html>
+<?php
+	session_start();
+	$_SESSION['user'] = $_POST['login'];
+	$user = $_SESSION['user'];
+?>
 <HTML>
 
 <HEAD>
 <TITLE>Login</TITLE>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="LPageStyle.css">
 </HEAD>
 
 <BODY class="registerBody">
-	<DIV class="registerHeader">
+	<DIV class="header">
 		<H2>Login</H2>
 	</DIV>
-	<FORM class="registerForm" method="post" action="Login.php">
+	<FORM class="registerForm" method="post" action= "dashboard.php">
 
 	<DIV class="input-group">
-		<label>Username</label>
-		<input type="text" name="username" value="">
+		<label>Email</label>
+		<input type='hidden' name = 'op' value='login'>
+		<input type="text" name="login" value="">
 	</DIV>
 	
 	<DIV class="input-group">
 		<label>Password</label>
-		<input type="password" name="password">
+		<input type='hidden' name = 'pass' value=''>
+		<input type="text" name="password" value="">
 	</DIV>
 	
 	<DIV class="input-group">
-		<button type="submit" class="btn" name="login_btn">Login</button>
+	<button type="submit" class="btn" value="login"> Login</button>
 	</DIV>
+	
+	<!--DIV class="input-group">
+		<button type="submit" class="btn" value="login"> <A href="StudentDashboard.php?<?php print "user = $user"?>">Student Login </A></button>
+	</DIV-->
 	
 	<p>
 		Need an Account? <a href="register.php">Sign Up</a>

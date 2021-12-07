f2dbfbcdfab89c164f56d56d650b8ab3fdac9982
thecritic314 <62189841+thecritic314@users.noreply.github.com>
2021-12-07 02:59:11 -0500

    Update LandingPage.php

--- LandingPage.php
+++ LandingPage.php
@@ -1,115 +1,46 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-<title>Landing Page </title>
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link type='text/css' rel='stylesheet' href="LPageStyle.css" />
-
+	<title>Registration system PHP and MySQL</title>
+ <link rel="stylesheet" href="LPageStyle.css">
 </head>
-
 <body>
 
+<h1 style="color:white";><center>Gettysburg College</center></h1>
 
-<div class="navbar">
-  <a href="#home" onclick="openTab('Home', this, 'orange')" id="defaultOpen">HOME</a>
-  <div class="dropdown">
-    <button class="dropbtn">MENU
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#news" onclick="openTab('Signup', this)">Signup</a>
-      <a href="#news" onclick="openTab('Login', this)">Login</a>
-    </div>
-  </div>
+<style>
+body {
+  background-image: url('glatfelter2.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+</style>
+
+<div class='nav-brand'> 
+ <a href='home.php'> 
+   <img src='logo.png' alt='Official logo' width='100px' height='50px'> 
+ </a> 
+</div> 
+
+<div class="header">
+	<h2>TA Register</h2>
 </div>
-
-
-<img src='glatfelter2.jpg' /> 
-
-
-<div id="Home" class="tabcontent">
-  <h1><center>Event Brighter</center></h1>
-  <h4>Featured Events:</h4>
-
-  <div style="overflow-x:auto;">
-    <table>
-      <tr>
-        <th>Artist</th>
-        <th>Location</th>
-        <th>Date</th>
-        <th>Link</th>
-      </tr>
-      <tr>
-        <td>Travis Porter</td>
-        <td>Gettysburg College</td>
-        <td>4/26/18</td>
-        <td><button class="button" onclick="openTab('Events', this, 'blue')" style="vertical-align:middle" ><span>Purchase Tickets</span></button></td>
-      </tr>
-    </table>
-  </div>
-
-</div>
-  
-<div id="Signup" class="tabcontent">
-  <p><h1>Signup:</h1></p>
-  <FORM name ="fmSignup" method="POST" action="signup_f.php">
-  <table id="t1">
-<TR>
-<TD>UserID</TD>
-<TD> <INPUT type= "text" name="UserID" placeholder= "enter UserID"/> </TD>
-</TR>
-
-<TR>
-<TD>Password</TD>
-<TD> <INPUT type= "password" name="password" placeholder= "enter password"/> </TD>
-</TR>
-
-<TR>
-<TD>Email</TD>
-<TD> <INPUT type= "text" name="email" placeholder= "enter email"/> </TD>
-</TR>
-  
-<TR>
-<TD>Phone</TD>
-<TD> <INPUT type= "tel" name="phone" placeholder= "enter phone number"/> </TD>
-</TR>
-  
-<TR>
-<TD>Fname</TD>
-<TD> <INPUT type= "text" name="fname" placeholder= "enter first name"/> </TD>
-</TR>
-  
-<TR>
-<TD>Lname</TD>
-<TD> <INPUT type= "text" name="lname" placeholder= "enter last name"/> </TD>
-</TR>
-  
-<BR/>
-
-<TD>
-<INPUT type= "submit" value = "Sign Up" />
-<INPUT type= "reset" value = "Clear" />
-</TD>
-  
-</TABLE>
-</FORM>
-</div>
-
-<div id="Login" class="tabcontent">
-  <p><h1>Login:</h1></p>
-  <FORM name ='fmLogin' action='login_f.php' method='POST'>
-  <INPUT type='text' name='login' size='30' placeholder='Your Username' /> <BR /><BR />
-  <INPUT type='password' name='password' size='30' placeholder='Your Password' /> <BR /><BR />
-
-  <INPUT type='submit' value='Login'>
-  <INPUT type='reset' value='Clear'>
-  </FORM>
-</div>
-
-
-
+<form method="post" action="login.php">
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="username" value="">
+	</div>
+	<div class="input-group">
+		<label>Email</label>
+		<input type="email" name="email" value="">
+	</div>
+	<div class="input-group">
+		<button type="submit" class="btn" name="register_btn">Sign In</button>
+	</div>
+	<p>
+		Don't have an account? <a href="register.php">Register</a>
+	</p>
+</form>
 </body>
 </html>
-
